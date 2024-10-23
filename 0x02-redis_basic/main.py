482a80348dec9384a432bb27ff6452efb0fbba27
rexmannII <onyegbulerexejike@gmail.com>
2024-10-23 23:54:39 +0000

    updated_main.py

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,27 +1,48 @@
 #!/usr/bin/env python3
-""" Main file """
+"""
+Main file
+"""
+import redis
 
 Cache = __import__('exercise').Cache
 
-        cache = Cache()
+cache = Cache()
 
-        cache.store(b"first")
-        print(cache.get(cache.store.__qualname__))
+data = b"hello"
+key = cache.store(data)
+print(key)
 
-        cache.store(b"second")
-        cache.store(b"third")
-        print(cache.get(cache.store.__qualname__))
+local_redis = redis.Redis()
+print(local_redis.get(key))
 
+cache = Cache()
 
-        s1 = cache.store("first")
-        print(s1)
-        s2 = cache.store("secont")
-        print(s2)
-        s3 = cache.store("third")
-        print(s3)
+TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8") 
+}
 
-        inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
-        outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
 
-        print("inputs: {}".format(inputs))
-        print("outputs: {}".format(outputs))
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
+
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
+
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
